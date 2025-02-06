0a454493e7d1e1bf5a7a93738571bbb15158a97a
Edeilton Oliveira <edeiltonso@gmail.com>
2025-02-06 17:04:29 -0300

    fix: corrige implementação do timer

--- embarcatech-one-shot-timer.c
+++ embarcatech-one-shot-timer.c
@@ -1,82 +1,67 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "pico/time.h"
-
-#define RED_GPIO_PIN   13
-#define BLUE_GPIO_PIN  12
-#define GREEN_GPIO_PIN 11
+#include "hardware/timer.h"
+#include "hardware/clocks.h"
 
 #define BUTTON_GPIO_PIN 5
 
 #define STATUS_TIME_MS 3000
 
-#define DEBOUNCING_TIME_MS 500
+#define DEBOUNCING_TIME_MS 200
 
-bool is_red_active = false;
-bool is_green_active = false;
-bool is_blue_active = false;
+uint8_t leds_gpio_pins[] = {12, 13, 11}; // ordenados pela sequência de desligamento
 
-int64_t turn_off_red_callback(alarm_id_t id, void *user_data) {
-    gpio_put(RED_GPIO_PIN, false);
-    is_red_active = false;
-    return 0;
-}
-
-int64_t turn_off_green_callback(alarm_id_t id, void *user_data) {
-    gpio_put(GREEN_GPIO_PIN, false);
-    is_green_active = false;
-    return 0;
-}
-
-int64_t turn_off_blue_callback(alarm_id_t id, void *user_data) {
-    gpio_put(BLUE_GPIO_PIN, false);
-    is_blue_active = false;
-    return 0;
-}
-
+uint8_t active_leds = 0;
 uint64_t last_time = 0;
+
+int64_t turn_off_callback(alarm_id_t id, void *user_data) {
+    
+    if (active_leds)
+    {
+        gpio_put(leds_gpio_pins[3-active_leds], false);
+        active_leds--;
+    }
+
+    if (active_leds) add_alarm_in_ms(STATUS_TIME_MS, turn_off_callback, NULL, false);
+
+    return 0;
+}
 
 int main() {
     stdio_init_all();
 
-    // inicializa pinos
-    gpio_init(RED_GPIO_PIN);
-    gpio_init(BLUE_GPIO_PIN);
-    gpio_init(GREEN_GPIO_PIN);
-    gpio_set_dir(RED_GPIO_PIN, GPIO_OUT);
-    gpio_set_dir(BLUE_GPIO_PIN, GPIO_OUT);
-    gpio_set_dir(GREEN_GPIO_PIN, GPIO_OUT);
+    // inicializa pinos dos LEDs
+    for (uint8_t i = 0; i < 3; i++)
+    {
+        gpio_init(leds_gpio_pins[i]);
+        gpio_set_dir(leds_gpio_pins[i], GPIO_OUT);
+        gpio_put(leds_gpio_pins[i], false);
+    }
 
+    // inicializa pino do botão
     gpio_init(BUTTON_GPIO_PIN);
     gpio_set_dir(BUTTON_GPIO_PIN, GPIO_IN);
     gpio_pull_up(BUTTON_GPIO_PIN);
 
     while (true) {
-        bool is_button_pressed = !gpio_get(BUTTON_GPIO_PIN);
+        uint32_t current_time = to_ms_since_boot(get_absolute_time());
 
-        // se o botão estiver pressionado E cada um dos LEDs estiver apagado
-        if (is_button_pressed && !is_red_active && !is_green_active && !is_blue_active) {
-            uint64_t current_time = to_ms_since_boot(get_absolute_time());
+        // trata o efeito bouncing com um atraso de DEBOUNCING_TIME_MS milissegundos
+        if(current_time - last_time > DEBOUNCING_TIME_MS)
+        {
+            bool is_button_pressed = !gpio_get(BUTTON_GPIO_PIN);
 
-            // trata o efeito bouncing com um atraso de DEBOUNCING_TIME_MS milissegundos
-            if (current_time - last_time > DEBOUNCING_TIME_MS)
+            // se o botão estiver pressionado e todos os LEDs estiverem apagados
+            if (is_button_pressed && !active_leds)
             {
                 last_time = current_time;
 
                 // liga os LEDs
-                gpio_put(RED_GPIO_PIN, true);
-                gpio_put(GREEN_GPIO_PIN, true);
-                gpio_put(BLUE_GPIO_PIN, true);
+                for(uint8_t i = 0; i < 3; i++) gpio_put(leds_gpio_pins[i], true);
+                active_leds = 3;
 
-                // atualiza status de cada um dos LEDs
-                is_red_active = true;
-                is_green_active = true;
-                is_blue_active = true;
-
-                // programa alarmes para desligar cada LED de forma independente
-                add_alarm_in_ms(STATUS_TIME_MS, turn_off_blue_callback, NULL, false);
-                add_alarm_in_ms(STATUS_TIME_MS*2, turn_off_red_callback, NULL, false);
-                add_alarm_in_ms(STATUS_TIME_MS*3, turn_off_green_callback, NULL, false);
+                // programa alarme para desligar o primeiro LED
+                add_alarm_in_ms(STATUS_TIME_MS, turn_off_callback, NULL, false);
             }
         }
     }
